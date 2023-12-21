c5be0e0e0794b2f1704fcfc3239227eb57987f55
caducoder <cadudrop@hotmail.com>
2023-12-21 18:08:55 -0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
