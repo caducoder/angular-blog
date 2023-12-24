0c36443cf20b7611eb8a945e06623bada326c80a
caducoder <cadudrop@hotmail.com>
2023-12-24 18:18:27 -0300

    feat: add big-title component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BigTitleComponent } from './components/big-title/big-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BigCardComponent,
     SmallCardComponent,
-    NavbarComponent
+    NavbarComponent,
+    BigTitleComponent
   ],
   imports: [
     BrowserModule,
